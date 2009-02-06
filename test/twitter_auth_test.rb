8ff27ac7ad7ee7ae7a8ad3f24a6181a182dbd5bc
Michael Bleigh <michael@intridea.com>
2009-02-06 15:52:56 -0500

    Removing things.

--- test/twitter_auth_test.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/test_helper'
-
-class TwitterAuthTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
