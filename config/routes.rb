fa9b42c7960dc4d38cd5e0617c807d03e6259460
Michael Bleigh <michael@intridea.com>
2009-03-17 12:03:47 -0400

    Fixed up authentication callbacks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.login '/login', :controller => 'sessions', :action => 'new'
   map.resource :session
   map.oauth_callback '/oauth_callback', :controller => 'sessions', :action => 'oauth_callback'
 end
